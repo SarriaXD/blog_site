9d591a795f841c0c19da9ec388af604f9c491bf3
SarriaXD <sarria.qi.wang@gmail.com>
2024-04-08 13:05:29 -0500

    finish the top nav bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { TopNavBar } from "./components/nav/TopNavBar.tsx";
+import { HomeScreen } from "./components/HomeScreen.tsx";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    html {
+         background-color: rgba(28, 28, 28);
+    }
+
+     body {
+        min-height: 150vh;
+        background: linear-gradient(45deg, #1c1c1c, #0F0F0F);
+        color: white;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+`;
+
 function App() {
   return (
     <>
-      <h1>Test</h1>
+      <GlobalStyle />
+      <TopNavBar />
+      <HomeScreen />
     </>
   );
 }
---
+++ src/components/nav/TopNavBar.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 80px;
+  width: 100%;
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  background-color: rgba(28, 28, 28, 0.91);
+  display: flex;
+  align-items: center;
+  box-shadow: 3px, 3px, 3px, rgba(0, 0, 0, 0.1);
+`;
+
+const NavLogo = styled.img`
+  height: 100%;
+  padding: 15px 30px;
+  display: flex;
+  text-decoration: none;
+  align-items: center;
+`;
+
+const NavList = styled.ul`
+  height: 100%;
+  display: flex;
+  list-style: none;
+`;
+
+const NavItem = styled.li`
+  height: 100%;
+`;
+
+const NavLink = styled.a`
+  height: 100%;
+  padding: 0 30px;
+  display: flex;
+  text-decoration: none;
+  align-items: center;
+  color: white;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #333;
+  }
+`;
+
+export const TopNavBar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLogo src="/logo.png" alt="logo" />
+        <NavList>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/about">About</NavLink>
+          </NavItem>
+        </NavList>
+      </Nav>
+    </>
+  );
+};
