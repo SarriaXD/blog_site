24a4e6fb93985db41a1d6c086eba169d5d6afa90
SarriaXD <sarria.qi.wang@gmail.com>
2024-05-11 07:12:04 -0500

    add Parallax Effect to Hero Section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,10 @@
 import { MyTech } from '../components/home/MyTech.tsx'
 import { HeroSection } from '../sections/home/HeroSection.tsx'
-import { MasonryGridGallery } from '../components/home/MasonryGridGallery.tsx'
 
 export function Homepage() {
     return (
         <>
             <HeroSection />
-            <MasonryGridGallery />
             <MyTech />
         </>
     )
--- src/sections/home/HeroSection.tsx
+++ src/sections/home/HeroSection.tsx
@@ -1,15 +1,33 @@
 import { Typography } from '@material-tailwind/react'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
+
+const MotionTypography = motion(Typography)
 
 export const HeroSection = () => {
+    const ref = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ['start 80px', 'end start'],
+    })
+    const titleY = useTransform(scrollYProgress, [0, 1], [0, -300])
+    const subtitleY = useTransform(scrollYProgress, [0, 1], [0, -200])
     return (
         <>
             <section
+                ref={ref}
                 className="flex flex-col items-center gap-4
              p-8 text-center
              md:py-8 md:gap-8
              xl:py-20 xl:gap-12"
             >
-                <Typography
+                <MotionTypography
+                    animate={{
+                        opacity: [0, 1],
+                        scale: [0.8, 1],
+                        transition: { duration: 1.5 },
+                    }}
+                    style={{ y: titleY }}
                     variant="h1"
                     className="text-4xl
                     md:text-6xl md:max-w-screen-sm
@@ -19,8 +37,14 @@ export const HeroSection = () => {
                         I'm Qi
                     </span>
                     , a software engineer based in Canada.
-                </Typography>
-                <Typography
+                </MotionTypography>
+                <MotionTypography
+                    animate={{
+                        opacity: [0, 1],
+                        scale: [0.8, 1],
+                        transition: { duration: 1.5, delay: 0.5 },
+                    }}
+                    style={{ y: subtitleY }}
                     variant="h2"
                     color="gray"
                     textGradient={true}
@@ -29,7 +53,7 @@ export const HeroSection = () => {
                     xl:text-4xl"
                 >
                     I specialize in full-stack development.
-                </Typography>
+                </MotionTypography>
             </section>
         </>
     )
