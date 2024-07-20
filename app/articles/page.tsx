9a2d87f54ba11af27fccd9c113099845c68473c6
SarriaXD <sarria.qi.wang@gmail.com>
2024-07-20 00:41:13 -0500

    add articles route

---
+++ app/articles/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+    return (
+        <main>
+            <div className="h-20" />
+            <ul>
+                <li>Android</li>
+                <li>Dart</li>
+                <li>Flutter</li>
+                <li>Java</li>
+                <li>Jetpack Compose</li>
+                <li>Kotlin</li>
+                <li>React</li>
+                <li>Spring Boot</li>
+            </ul>
+        </main>
+    )
+}
