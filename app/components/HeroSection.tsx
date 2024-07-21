88fc97abd350835b0a9e653013099b6e527ad921
SarriaXD <sarria.qi.wang@gmail.com>
2024-07-21 00:41:03 -0500

    move IntroductionSection.tsx section into HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,13 +1,87 @@
-import { IntroductionSection } from './IntroductionSection.tsx'
-import { TechStackSection } from './TechStackSection.tsx'
+'use client'
+
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion'
+import { Typography } from '@material-tailwind/react'
+import { useRef } from 'react'
+
+const MotionTypography = motion(Typography)
+
+export const Introduction = () => {
+    const ref = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ['start 80px', 'end start'],
+    })
+    const smoothScrollYProgress = useSpring(scrollYProgress, {
+        stiffness: 200,
+        damping: 20,
+    })
+    const smoothTitleY = useTransform(smoothScrollYProgress, [0, 1], [0, -300])
+    const smoothSubtitleY = useTransform(
+        smoothScrollYProgress,
+        [0, 1],
+        [0, -200]
+    )
+    return (
+        <div
+            ref={ref}
+            className="container mx-auto flex flex-col gap-4 p-2 text-center md:gap-8 md:p-4 xl:gap-12 xl:p-8"
+        >
+            <MotionTypography
+                initial={{
+                    opacity: 0,
+                    y: 50,
+                }}
+                animate={{
+                    opacity: 1,
+                    y: 0,
+                }}
+                transition={{
+                    type: 'tween',
+                    duration: 0.6,
+                    delay: 0.2,
+                }}
+                style={{ y: smoothTitleY }}
+                variant="h1"
+                className="text-4xl md:text-6xl xl:text-8xl"
+            >
+                <span className="bg-hero-text-gradient bg-clip-text text-transparent">
+                    I'm Qi
+                </span>
+                , a software engineer based in Canada.
+            </MotionTypography>
+            <MotionTypography
+                initial={{
+                    opacity: 0,
+                    y: 100,
+                }}
+                animate={{
+                    opacity: 1,
+                    y: 0,
+                }}
+                transition={{
+                    type: 'tween',
+                    duration: 0.6,
+                    delay: 0.5,
+                }}
+                style={{ y: smoothSubtitleY }}
+                variant="h2"
+                color="gray"
+                textGradient={true}
+                className="text-2xl md:text-3xl xl:text-5xl"
+            >
+                I specialize in full-stack development.
+            </MotionTypography>
+        </div>
+    )
+}
 
 export const HeroSection = () => {
     return (
         <>
             <section className="bg-hero-section-gradient ">
                 <div className="h-20" />
-                <IntroductionSection />
-                <TechStackSection />
+                <Introduction />
             </section>
         </>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from './components/HeroSection.tsx'
 import { ProjectListSection } from './components/ProjectListSection.tsx'
+import { TechStackSection } from './components/TechStackSection.tsx'
 
 export default function Page() {
     return (
         <main>
             <HeroSection />
+            <TechStackSection />
             <ProjectListSection />
             <section className="h-[2000px]" />
         </main>
