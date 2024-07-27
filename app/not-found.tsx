1aa36d63bdc3e4df0ba61079bd2b0d4db6b9cd17
SarriaXD <sarria.qi.wang@gmail.com>
2024-07-27 05:18:47 -0500

    add 404 not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import { not_found } from '../assets/images'
+import { Button, Typography } from './components/Material.tsx'
+import Link from 'next/link'
+
+const NotFoundImage = () => {
+    return (
+        <div className="max-w-[80%] md:h-full">
+            <Image src={not_found} alt="404 not fount image" />
+        </div>
+    )
+}
+
+const NotFoundText = () => {
+    return (
+        <div className="flex flex-shrink-0 flex-col items-center justify-center gap-4 self-center xl:gap-8">
+            <Typography variant={'h1'} color="white" className="text-8xl">
+                Opps!
+            </Typography>
+            <Typography variant={'h2'} color={'white'}>
+                Page not found
+            </Typography>
+            <Link href="/">
+                <Button color="gray">Go back to home</Button>
+            </Link>
+        </div>
+    )
+}
+
+export default function NotFound() {
+    return (
+        <main>
+            <div className="flex min-h-[100vh] items-center bg-black px-8 py-24 xl:px-16">
+                <div className="flex flex-col items-center gap-8 md:flex-row xl:gap-32">
+                    <NotFoundImage />
+                    <NotFoundText />
+                </div>
+            </div>
+        </main>
+    )
+}
