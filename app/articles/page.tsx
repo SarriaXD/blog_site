9d8a594a0d786e0a766e235f7ac44134ecce0823
SarriaXD <sarria.qi.wang@gmail.com>
2024-07-27 07:22:24 -0500

    redirect articles to 404.

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,19 +1,5 @@
+import { notFound } from 'next/navigation'
+
 export default function Page() {
-    return (
-        <main>
-            <div className="min-h-[100vh]">
-                <div className="h-20" />
-                <ul>
-                    <li>Android</li>
-                    <li>Dart</li>
-                    <li>Flutter</li>
-                    <li>Java</li>
-                    <li>Jetpack Compose</li>
-                    <li>Kotlin</li>
-                    <li>React</li>
-                    <li>Spring Boot</li>
-                </ul>
-            </div>
-        </main>
-    )
+    notFound()
 }
