e3435bf7850e16da2cf27cfbf7b11a0b77851d99
SarriaXD <sarria.qi.wang@gmail.com>
2024-08-07 21:58:26 -0500

    add animation for FlutterProjectSection.tsx.

--- app/components/FlutterProjectSection.tsx
+++ app/components/FlutterProjectSection.tsx
@@ -15,16 +15,49 @@ import {
 import { useRef, useState } from 'react'
 import { useMediaQuery } from '../hooks.ts'
 
+const useTitleAnimation = () => {
+    const ref = useRef(null)
+    const [enterViewport, setEnterViewport] = useState(false)
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ['end end', 'end start'],
+    })
+    useMotionValueEvent(scrollYProgress, 'change', (progress) => {
+        if (progress > 0.0 && !enterViewport) {
+            setEnterViewport(true)
+        }
+        if (progress === 0.0 && enterViewport) {
+            setEnterViewport(false)
+        }
+    })
+    return {
+        ref,
+        enterViewport,
+    }
+}
+
 const Title = () => {
+    const { ref, enterViewport } = useTitleAnimation()
     return (
-        <div className="mx-auto max-w-[290px] md:w-[578px] md:max-w-full lg:w-[735px]">
+        <motion.div
+            ref={ref}
+            className="mx-auto max-w-[290px] md:w-[578px] md:max-w-full lg:w-[735px]"
+            animate={{
+                opacity: enterViewport ? 1 : 0,
+                y: enterViewport ? 0 : 50,
+            }}
+            transition={{
+                type: 'spring',
+                duration: 1,
+            }}
+        >
             <h3 className="text-3xl text-[#86868b] md:text-6xl lg:text-8xl">
                 Driving Test App.
             </h3>
             <h4 className="text-3xl text-[#F5F5F7] md:text-6xl lg:text-8xl">
                 Powered by Flutter.
             </h4>
-        </div>
+        </motion.div>
     )
 }
 
@@ -51,56 +84,68 @@ const ExploreStickyButton = () => {
     )
 }
 
-const useFlutterProjectImageGalleryAnimation = () => {
+interface ImageGalleryProps {
+    nearTopViewport: boolean
+}
+
+const useImageGalleryAnimation = () => {
     const ref = useRef(null)
-    const [isInView, setIsInView] = useState(false)
+    const [enterViewport, setEnterViewport] = useState(false)
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ['center end', 'center center'],
+        offset: ['center end', 'end start'],
     })
-    const scale = useTransform(scrollYProgress, [0, 1], [2, 1])
     useMotionValueEvent(scrollYProgress, 'change', (progress) => {
-        if (progress === 1 && !isInView) {
-            setIsInView(true)
+        if (progress > 0.0 && !enterViewport) {
+            setEnterViewport(true)
         }
-        if (progress < 1 && isInView) {
-            setIsInView(false)
+        if (progress === 0.0 && enterViewport) {
+            setEnterViewport(false)
         }
     })
     return {
         ref,
-        scale,
-        isInView,
+        enterViewport,
     }
 }
 
-const ImageGallery = () => {
-    const { ref, scale, isInView } = useFlutterProjectImageGalleryAnimation()
+const ImageGallery = ({ nearTopViewport }: ImageGalleryProps) => {
+    const { ref, enterViewport } = useImageGalleryAnimation()
     return (
-        <div
+        <motion.div
             ref={ref}
             className="relative z-10 mx-auto mt-8 flex w-[92%] items-center justify-between gap-4 md:mt-0 md:w-full md:gap-6 lg:gap-8"
+            animate={{
+                opacity: enterViewport ? 1 : 0,
+                y: enterViewport ? 0 : 100,
+            }}
             style={{
                 perspective: '2000px',
+            }}
+            transition={{
+                type: 'spring',
+                duration: 1,
             }}
         >
             <motion.div
                 className="z-0"
                 animate={{
-                    opacity: isInView ? 1 : 0,
-                    y: isInView ? 0 : 20,
+                    opacity: nearTopViewport ? 1 : 0,
+                    x: nearTopViewport ? 0 : 100,
                 }}
                 transition={{
                     type: 'spring',
-                    duration: 1.2,
+                    duration: nearTopViewport ? 1 : 0.5,
                 }}
             >
                 <Image src={flutter_project_1} alt="flutter project image 1" />
             </motion.div>
             <motion.div
                 className="z-10"
+                animate={{
+                    scale: nearTopViewport ? 1 : 2,
+                }}
                 style={{
-                    scale,
                     transformOrigin: 'top',
                 }}
                 transition={{
@@ -113,58 +158,36 @@ const ImageGallery = () => {
             <motion.div
                 className="z-0"
                 animate={{
-                    opacity: isInView ? 1 : 0,
-                    y: isInView ? 0 : 20,
+                    opacity: nearTopViewport ? 1 : 0,
+                    x: nearTopViewport ? 0 : -100,
                 }}
                 transition={{
                     type: 'spring',
-                    duration: 1,
+                    duration: nearTopViewport ? 1 : 0.5,
                 }}
             >
                 <Image src={flutter_project_1} alt="flutter project image 1" />
             </motion.div>
-        </div>
+        </motion.div>
     )
-}
-
-const useFlutterProjectIntroductionAnimation = () => {
-    const ref = useRef(null)
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ['start end', 'start 80%'],
-    })
-    const [isInView, setIsInView] = useState(false)
-    useMotionValueEvent(scrollYProgress, 'change', (progress) => {
-        if (progress === 1 && !isInView) {
-            setIsInView(true)
-        }
-        if (progress < 1 && isInView) {
-            setIsInView(false)
-        }
-    })
-    return {
-        ref,
-        isInView,
-    }
 }
 
 interface IntroductionProps {
     progress: MotionValue<number>
+    enterViewport: boolean
 }
 
-const Introduction = ({ progress }: IntroductionProps) => {
-    const { ref, isInView } = useFlutterProjectIntroductionAnimation()
+const Introduction = ({ progress, enterViewport }: IntroductionProps) => {
     return (
         <motion.div
-            ref={ref}
             animate={{
-                opacity: isInView ? 1 : 0,
-                y: isInView ? 0 : 50,
+                opacity: enterViewport ? 1 : 0,
+                y: enterViewport ? 0 : 50,
             }}
             transition={{
                 type: 'spring',
-                delay: isInView ? 0.5 : 0,
-                duration: 1.2,
+                delay: enterViewport ? 0.5 : 0,
+                duration: enterViewport ? 1 : 0.5,
             }}
             className="relative z-0 p-8"
         >
@@ -191,7 +214,6 @@ interface ProgressBarProps {
 const ProgressBar = ({ progress }: ProgressBarProps) => {
     const isMobile = useMediaQuery('(max-width: 735px)', true)
     const percentage = useTransform(progress, [0, 1], ['0%', '100%'])
-    console.log(progress)
     return (
         <div className="h-full w-full rounded-full bg-gray-800">
             {isMobile ? (
@@ -215,17 +237,46 @@ const ProgressBar = ({ progress }: ProgressBarProps) => {
     )
 }
 
+const useMainContentAnimation = () => {
+    const ref = useRef(null)
+    const [nearTopViewport, setNearTopViewport] = useState(false)
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ['start end', 'start start'],
+    })
+    useMotionValueEvent(scrollYProgress, 'change', (progress) => {
+        console.log('progress', progress)
+        if (progress === 1 && !nearTopViewport) {
+            setNearTopViewport(true)
+        }
+        if (progress < 1 && nearTopViewport) {
+            setNearTopViewport(false)
+        }
+    })
+    return {
+        ref,
+        nearTopViewport,
+    }
+}
+
 interface MainContentProps {
     progress: MotionValue<number>
 }
 
 const MainContent = ({ progress }: MainContentProps) => {
+    const { ref, nearTopViewport } = useMainContentAnimation()
     return (
-        <div className="sticky top-0 mx-auto h-[100vh] min-h-[900px] md:w-[692px] lg:w-[800px]">
+        <div
+            ref={ref}
+            className="sticky top-0 mx-auto h-[100vh] min-h-[900px] md:w-[692px] lg:w-[800px]"
+        >
             <ExploreStickyButton />
             <div className="mx-auto h-full max-w-[530px] py-20 md:max-w-full">
-                <ImageGallery />
-                <Introduction progress={progress} />
+                <ImageGallery nearTopViewport={nearTopViewport} />
+                <Introduction
+                    progress={progress}
+                    enterViewport={nearTopViewport}
+                />
             </div>
         </div>
     )
