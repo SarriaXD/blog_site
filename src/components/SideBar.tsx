e39764cb7e8b505d5f1d0f721745a0bccb887fc3
SarriaXD <sarria.qi.wang@gmail.com>
2024-08-15 06:49:50 -0500

    rewrite SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
-    Drawer,
     IconButton,
     List,
     ListItem,
     ListItemSuffix,
 } from '@components/Material.tsx'
 import Link from 'next/link'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { Close, Dog, Email, Github, Linkedin } from '@public/icons'
+import { useMediaQuery } from '@hooks/hooks.ts'
 
-interface SideBarProps {
-    open: boolean
-    onClose: () => void
+const itemVariants = {
+    initial: { x: '-100%' },
+    enter: (i: number) => ({
+        x: 0,
+        transition: {
+            duration: 0.8,
+            ease: [0.76, 0, 0.24, 1],
+            delay: 0.05 * i,
+        },
+    }),
+    exit: (i: number) => ({
+        x: '-100%',
+        transition: {
+            duration: 0.8,
+            ease: [0.76, 0, 0.24, 1],
+            delay: 0.05 * i,
+        },
+    }),
 }
 
-const internalLinks = [
+const sideBarVariants = {
+    initial: { x: '-100%' },
+    enter: { x: 0, transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] } },
+    exit: {
+        x: '-100%',
+        transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    },
+}
+
+const links: {
+    href: string
+    icon?: 'email' | 'linkedin' | 'github' | undefined
+    newTab: boolean
+    text: string
+}[] = [
     { href: '/articles', text: 'Articles', newTab: false },
     {
         href: 'https://github.com/SarriaXD?tab=repositories',
@@ -23,36 +52,6 @@ const internalLinks = [
         newTab: true,
     },
     { href: '/tools', text: 'Tools', newTab: false },
-]
-
-interface InternalLinkProps {
-    href: string
-    text: string
-    newTab: boolean
-    onClose: () => void
-}
-
-const InternalLink = ({ href, text, newTab, onClose }: InternalLinkProps) => {
-    const linkProps = {
-        href,
-        onClick: onClose,
-        ...(newTab ? { target: '_blank', rel: 'noopener noreferrer' } : {}),
-    }
-    return (
-        <Link {...linkProps}>
-            <ListItem>
-                <span className="text-base text-white">{text}</span>
-            </ListItem>
-        </Link>
-    )
-}
-
-const externalLinks: {
-    href: string
-    icon: 'email' | 'linkedin' | 'github'
-    newTab: boolean
-    text: string
-}[] = [
     {
         href: 'mailto:sarria.qi.wang@gmail.com',
         icon: 'email',
@@ -72,18 +71,7 @@ const externalLinks: {
         text: 'GitHub',
     },
 ]
-
-interface ExternalLinkProps {
-    href: string
-    icon: 'email' | 'linkedin' | 'github'
-    text: string
-    newTab: boolean
-    index: number
-    open: boolean
-    onClose: () => void
-}
-
-const ExternalIcon = ({ icon }: { icon: 'email' | 'linkedin' | 'github' }) => {
+const LinkIcon = ({ icon }: { icon: 'email' | 'linkedin' | 'github' }) => {
     switch (icon) {
         case 'email':
             return <Email className="size-8 text-white" />
@@ -94,15 +82,23 @@ const ExternalIcon = ({ icon }: { icon: 'email' | 'linkedin' | 'github' }) => {
     }
 }
 
-const ExternalLink = ({
+interface LinkItemProps {
+    text: string
+    href: string
+    icon?: 'email' | 'linkedin' | 'github' | undefined
+    newTab: boolean
+    index: number
+    onClose: () => void
+}
+
+const LinkItem = ({
+    text,
     href,
     icon,
-    text,
     newTab,
     index,
-    open,
     onClose,
-}: ExternalLinkProps) => {
+}: LinkItemProps) => {
     const linkProps = {
         href,
         onClick: onClose,
@@ -111,75 +107,80 @@ const ExternalLink = ({
     return (
         <Link {...linkProps}>
             <motion.div
-                animate={{
-                    x: open ? 0 : '-100%',
-                }}
-                transition={{
-                    type: 'spring',
-                    stiffness: 120,
-                    damping: 12,
-                    delay: index * 0.2,
-                }}
+                variants={itemVariants}
+                custom={index}
+                initial="initial"
+                animate="enter"
+                exit="exit"
             >
                 <ListItem>
                     <span className="text-base text-white">{text}</span>
-                    <ListItemSuffix>
-                        <ExternalIcon icon={icon} />
-                    </ListItemSuffix>
+                    {icon && (
+                        <ListItemSuffix>
+                            <LinkIcon icon={icon} />
+                        </ListItemSuffix>
+                    )}
                 </ListItem>
             </motion.div>
         </Link>
     )
 }
 
+interface SideBarProps {
+    open: boolean
+    onClose: () => void
+}
+
 export function SideBar({ open, onClose }: SideBarProps) {
+    const isMobile = useMediaQuery('(max-width: 735px)')
+    useEffect(() => {
+        if (!isMobile) onClose()
+    }, [onClose, isMobile])
     return (
         <nav>
-            <Drawer
-                open={open}
-                onClose={onClose}
-                size={720}
-                overlay={false}
-                className="bg-gray-900 bg-opacity-80 backdrop-blur-md will-change-transform"
-            >
-                <div className="flex items-center justify-between p-4">
-                    <Link href="/">
-                        <IconButton
-                            size="lg"
-                            aria-label="Back To Home"
-                            className="bg-transparent"
-                            onClick={onClose}
-                        >
-                            <Dog className="size-8 text-white" />
-                        </IconButton>
-                    </Link>
-                    <IconButton
-                        aria-label="Close Side Bar"
-                        variant="text"
+            <AnimatePresence mode="wait">
+                {open && (
+                    <motion.div
+                        key="slide"
+                        initial="initial"
+                        animate="enter"
+                        exit="exit"
+                        variants={sideBarVariants}
                         onClick={onClose}
+                        className="fixed left-0 top-0 z-50 h-[100vh] w-full bg-black bg-opacity-[0.75] backdrop-blur-[20px] will-change-transform"
                     >
-                        <Close className="size-6 text-white" />
-                    </IconButton>
-                </div>
-                <List>
-                    {internalLinks.map((link) => (
-                        <InternalLink
-                            key={link.text}
-                            {...link}
-                            onClose={onClose}
-                        />
-                    ))}
-                    {externalLinks.map((link) => (
-                        <ExternalLink
-                            key={link.text}
-                            {...link}
-                            index={externalLinks.indexOf(link)}
-                            open={open}
-                            onClose={onClose}
-                        />
-                    ))}
-                </List>
-            </Drawer>
+                        <div className="flex items-center justify-between p-4">
+                            <Link href="/">
+                                <IconButton
+                                    size="lg"
+                                    aria-label="Back To Home"
+                                    className="bg-transparent"
+                                    onClick={onClose}
+                                >
+                                    <Dog className="size-8 text-white" />
+                                </IconButton>
+                            </Link>
+                            <IconButton
+                                aria-label="Close Side Bar"
+                                variant="text"
+                                onClick={onClose}
+                            >
+                                <Close className="size-6 text-white" />
+                            </IconButton>
+                        </div>
+                        <List>
+                            {links.map((link) => (
+                                <LinkItem
+                                    key={link.text}
+                                    {...link}
+                                    index={links.indexOf(link)}
+                                    onClose={onClose}
+                                />
+                            ))}
+                        </List>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </nav>
     )
 }
