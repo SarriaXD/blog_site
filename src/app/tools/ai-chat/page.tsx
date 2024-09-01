b1347d50723de1b18d923f54944b5446e5e4a227
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-01 05:29:04 -0500

    add next.js ai package

--- src/app/tools/ai-chat/page.tsx
+++ src/app/tools/ai-chat/page.tsx
@@ -1,16 +1,10 @@
 import ChatContent from './components/ChatContent.tsx'
-import ChatTextField from './components/ChatTextField.tsx'
 
 export default function Page() {
     return (
         <main>
             <section className="bg-[#02040A] pb-32 pt-24">
-                <div className="mx-auto px-8 md:max-w-[600px] lg:max-w-[800px]">
-                    <ChatContent />
-                </div>
-                <div className="fixed bottom-0 left-0 right-0 mx-auto px-8 md:max-w-[600px] lg:max-w-[800px]">
-                    <ChatTextField />
-                </div>
+                <ChatContent />
             </section>
         </main>
     )
