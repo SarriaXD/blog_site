b93fd235e9e8cf55459127250c1ce533a6aec9b1
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-05 08:27:47 -0500

    remove any type warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
