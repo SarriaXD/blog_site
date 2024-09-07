cd80e66f9aca4fd3b95b06212c98e29989397fca
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-07 01:17:57 -0500

    add warn toast.

--- src/components/ToastProvider.tsx
+++ src/components/ToastProvider.tsx
@@ -2,7 +2,22 @@
 
 import 'react-toastify/dist/ReactToastify.css'
 import '@styles/global.css'
-import { ToastContainer } from 'react-toastify'
+import { Slide, ToastContainer, ToastOptions } from 'react-toastify'
+import { Warn } from '@public/icons'
+
+export const toastErrorOptions: ToastOptions = {
+    type: 'error',
+    position: 'top-center',
+    transition: Slide,
+    className: 'bg-[#E96F40]',
+    icon: () => (
+        <div className="rounded-full bg-white p-1 text-[#E96F40]">
+            <Warn className="size-3" />
+        </div>
+    ),
+    autoClose: 3000,
+    hideProgressBar: true,
+}
 
 interface ToastProviderProps {
     children: React.ReactNode
@@ -12,7 +27,7 @@ export default function ToastProvider({ children }: ToastProviderProps) {
     return (
         <>
             {children}
-            <ToastContainer theme={'dark'} />
+            <ToastContainer className="pt-12" theme={'dark'} />
         </>
     )
 }
