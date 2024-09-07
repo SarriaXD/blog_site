b43b5225daf623dfa4ad83a90b160b7ac6b7f17e
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-07 18:14:15 -0500

    Provides a consistent background color for AI bot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className="bg-black text-white antialiased">
+            <body>
                 <ToastProvider>
                     <FPSCounter />
                     {children}
--- src/app/tools/ai-chat/page.tsx
+++ src/app/tools/ai-chat/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import ChatContent from './components/ChatContent.tsx'
 import { Header } from '@components/Header.tsx'
+import React from 'react'
+
 export default function Page() {
     return (
         <>
+            <style jsx global>{`
+                html {
+                    background-color: #212121;
+                }
+            `}</style>
             <Header />
-            <main className="size-full bg-[#212121]">
+            <main className="size-full">
                 <ChatContent />
             </main>
         </>
