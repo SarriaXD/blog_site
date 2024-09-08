bb89c0f4b2da17027aee164315486b733f72ee3a
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-08 08:43:46 -0500

    add background for empty messagePlaceholder

--- src/app/tools/ai-chat/components/EmptyMessagePlaceholder.tsx
+++ src/app/tools/ai-chat/components/EmptyMessagePlaceholder.tsx
@@ -2,7 +2,7 @@ import { Dog } from '@public/icons'
 
 const EmptyMessagePlaceholder = () => {
     return (
-        <div className="flex size-full flex-1 flex-col items-center justify-center gap-8">
+        <div className="flex size-full flex-1 flex-col items-center justify-center gap-8 bg-[#212121]">
             <Dog className="size-64 text-white" />
             <p className="text-center text-xl text-gray-300 md:text-2xl">
                 Hi I am Qi, how can I help you today?
